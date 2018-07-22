8704278adb46ce7f84e0a6370d9f1f264bd2d71c
ahyaemon <iamahyaemon@gmail.com>
2018-07-22 12:15:08 +0900

    fix doc

--- sphinx/copydocs.bat
+++ sphinx/copydocs.bat
@@ -8,7 +8,10 @@ rem /s: recurfsive
 rem /q: no confirmation
 rmdir /s/q  %DOCDIR%
 
-rem copy html to doc directory
+rem copy html to doc directory without unnesessary files
 rem /e: recursive
 rem /y: no confirmation
-xcopy /e/y %HTMLDIR%\* %DOCDIR%\
+xcopy /e/y/exclude:exclude.list %HTMLDIR%\* %DOCDIR%\
+
+rem create .nojekyll to ignore github jekyll encoding
+type nul > %DOCDIR%\.nojekyll
