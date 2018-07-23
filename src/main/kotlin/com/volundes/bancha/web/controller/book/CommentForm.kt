98897284b6ee229d881ee7111351c9a1e9e2f59c
ahyaemon <iamahyaemon@gmail.com>
2018-07-23 22:53:10 +0900

    バリデーションの実装

--- src/main/kotlin/com/volundes/bancha/web/controller/book/CommentForm.kt
+++ src/main/kotlin/com/volundes/bancha/web/controller/book/CommentForm.kt
@@ -1,11 +1,19 @@
 package com.volundes.bancha.web.controller.book
 
 import com.volundes.bancha.domain.dto.Comment
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
 
 data class CommentForm (
-        var sentenceId: Long,
-        var name: String,
-        var comment: String
+        var sentenceId: Long? = null,
+
+        @get:NotBlank
+        @get:Size(max = 255)
+        var name: String = "",
+
+        @get:NotBlank
+        @get:Size(max = 255)
+        var comment: String = ""
 ){
 
     fun toComment(): Comment{ return Comment(null, name, comment) }
