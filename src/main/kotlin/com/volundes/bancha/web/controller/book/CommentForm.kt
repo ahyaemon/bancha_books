8450e4bfb0db304681d914983afe900ec0d1e956
ahyaemon <iamahyaemon@gmail.com>
2018-07-23 23:06:24 +0900

    バリデーションメッセージの変更

--- src/main/kotlin/com/volundes/bancha/web/controller/book/CommentForm.kt
+++ src/main/kotlin/com/volundes/bancha/web/controller/book/CommentForm.kt
@@ -7,12 +7,12 @@ import javax.validation.constraints.Size
 data class CommentForm (
         var sentenceId: Long? = null,
 
-        @get:NotBlank
-        @get:Size(max = 255)
+        @get:NotBlank(message="名前が無い分け無いだろう")
+        @get:Size(max = 255, message="名前長すぎない？")
         var name: String = "",
 
-        @get:NotBlank
-        @get:Size(max = 255)
+        @get:NotBlank(message="コメントが無い分け無いだろう")
+        @get:Size(max = 255, message="コメント長すぎない？")
         var comment: String = ""
 ){
 
