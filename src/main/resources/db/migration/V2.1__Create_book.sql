ce9d75d0f2c2122c4e446e642afe307a134c3ca4
ahyaemon <iamahyaemon@gmail.com>
2018-07-24 07:44:02 +0900

    UNIQUEの追加

--- src/main/resources/db/migration/V2.1__Create_book.sql
+++ src/main/resources/db/migration/V2.1__Create_book.sql
@@ -3,6 +3,6 @@ CREATE TABLE book
     book_id serial NOT NULL,
     name character varying(255),
     author_id integer,
-    isbn character varying(255),
+    isbn character varying(255) UNIQUE,
     PRIMARY KEY (book_id)
 )
--- src/main/resources/db/migration/V7.1__Create_account.sql
+++ src/main/resources/db/migration/V7.1__Create_account.sql
@@ -1,7 +1,7 @@
 CREATE TABLE account
 (
     account_id serial NOT NULL,
-    name character varying(255),
+    name character varying(255) UNIQUE,
     pass character varying(255),
     role character varying(255),
     PRIMARY KEY (account_id)
