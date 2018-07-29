1e233c383434474aa80341d80a50c93d2088719e
ahyaemon <iamahyaemon@gmail.com>
2018-07-29 12:02:44 +0900

    migrationファイルのバージョンが違う場合のみflyway.clean実行

--- src/main/kotlin/com/volundes/bancha/init/flyway/FlywayCleanConfig.kt
+++ src/main/kotlin/com/volundes/bancha/init/flyway/FlywayCleanConfig.kt
@@ -1,13 +1,13 @@
 package com.volundes.bancha.init.flyway
 
 import org.flywaydb.core.Flyway
+import org.flywaydb.core.api.FlywayException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import javax.sql.DataSource
 
 @Configuration
-@Profile("production")
 class FlywayCleanConfig(
         private val dataSource: DataSource
 ) {
@@ -16,7 +16,15 @@ class FlywayCleanConfig(
     fun flywayClean(){
         val flyway = Flyway()
         flyway.setDataSource(dataSource)
-        flyway.clean()
+
+        // バージョンが違う場合、cleanする
+        try{
+            flyway.validate()
+        }
+        catch(e: FlywayException){
+            flyway.clean()
+        }
+
     }
 
 }
