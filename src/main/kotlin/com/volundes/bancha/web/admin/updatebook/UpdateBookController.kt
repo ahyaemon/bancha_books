4ab312b66a6f26a5556d37c83227c51c0af6ec39
ahyaemon <iamahyaemon@gmail.com>
2018-08-09 19:42:55 +0900

    本編集にもdialogを追加

--- src/main/kotlin/com/volundes/bancha/web/admin/updatebook/UpdateBookController.kt
+++ src/main/kotlin/com/volundes/bancha/web/admin/updatebook/UpdateBookController.kt
@@ -1,8 +1,10 @@
 package com.volundes.bancha.web.admin.updatebook
 
 import com.volundes.bancha.domain.admin.updatebook.UpdateBookService
+import com.volundes.bancha.web.book.CommentCountedBookItem
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
@@ -15,10 +17,23 @@ class UpdateBookController(
     fun login(
             model: Model
     ): String{
-        val updateBookItems = service.getBookInfos().map{ UpdateBookItem(it) }
-        model.addAttribute("updateBookItems", updateBookItems)
+        val updateBookMenus = service.getUpdateBookMenus()
+        val updateBookMenuItems = updateBookMenus.map{ UpdateBookMenuItem(it) }
+        model.addAttribute("updateBookMenuItems", updateBookMenuItems)
 
         return "admin/updatebook/index"
+    }
+
+    @RequestMapping("/{bookId}")
+    fun edit(
+        @PathVariable("bookId") bookId: String,
+        model: Model
+    ): String{
+        val book = service.getCommentCountedBookByBookId(bookId.toLong())
+        val bookItem = CommentCountedBookItem(book)
+        model.addAttribute("bookItem", bookItem)
+
+        return "admin/updatebook/edit"
     }
 
 }
---
+++ src/main/resources/static/js/updatebook.js
@@ -0,0 +1,17 @@
+function editTitle(){
+    MicroModal.show("modal-title", {
+        awaitCloseAnimation: true
+    });    
+}
+
+function editAuthor(){
+    MicroModal.show("modal-author", {
+        awaitCloseAnimation: true
+    });    
+}
+
+function editSentence(sentenceId){
+    MicroModal.show("modal-sentence", {
+        awaitCloseAnimation: true
+    });    
+}
