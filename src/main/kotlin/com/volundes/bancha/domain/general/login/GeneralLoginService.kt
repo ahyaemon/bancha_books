be44ed67059ed1da9803c7f5778d50ab39a81b74
ahyaemon <iamahyaemon@gmail.com>
2018-09-08 21:45:22 +0900

    gmailとの連携

--- src/main/kotlin/com/volundes/bancha/domain/general/login/GeneralLoginService.kt
+++ src/main/kotlin/com/volundes/bancha/domain/general/login/GeneralLoginService.kt
@@ -1,12 +1,28 @@
 package com.volundes.bancha.domain.general.login
 
+import org.simplejavamail.email.EmailBuilder
+import org.simplejavamail.mailer.MailerBuilder
+import org.simplejavamail.mailer.config.TransportStrategy
 import org.springframework.stereotype.Service
 
 @Service
 class GeneralLoginService {
 
     fun sendRegisterMail(mailAddress: String) {
+        val mail = EmailBuilder.startingBlank()
+                .to("me", "sht.ijm0011@gmail.com")
+                .from("banchabooks@gmail.com")
+                .withSubject("テストメールの送信です。")
+                .withPlainText("メール送信ができている？")
+                .buildEmail()
 
+        val mailer = MailerBuilder
+                .withSMTPServer("smtp.gmail.com", 465, "banchabooks@gmail.com", "ohaio98polish?gakumon")
+                .withTransportStrategy(TransportStrategy.SMTPS)
+                .withDebugLogging(true)
+                .buildMailer()
+
+        mailer.sendMail(mail)
     }
 
 }
