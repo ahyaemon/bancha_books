bc317b635ce671ad0cfb8b2146ae0783367d04da
ahyaemon <iamahyaemon@gmail.com>
2018-09-30 08:42:18 +0900

    本を追加

--- src/main/resources/db/migration/V21.2__Insert_menu.sql
+++ src/main/resources/db/migration/V21.2__Insert_menu.sql
@@ -1,7 +1,7 @@
 INSERT INTO menu
   (menu_id, title, subtitle, url)
 VALUES
-   (1, 'Books', 'メインコンテンツ', '/bookmenu')
-  ,(2, 'About', 'このページについて', '/about')
-  ,(3, 'Donation', '寄付', '/donation')
+  (1, 'Books', 'メインコンテンツ', '/bookmenu'),
+  (2, 'About', 'このページについて', '/about'),
+  (3, 'Donation', '寄付', '/donation')
 ;
